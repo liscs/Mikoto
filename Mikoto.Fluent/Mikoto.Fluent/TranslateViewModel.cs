74c8cbb52774d54818a4792927265a2172c41362
dreamZdew <dreamZdew@outlook.com>
2026-01-20 10:24:34 +0800

    添加翻译处理显示

--- Mikoto.Fluent/Mikoto.Fluent/TranslateViewModel.cs
+++ Mikoto.Fluent/Mikoto.Fluent/TranslateViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
 using Mikoto.DataAccess;
+using Mikoto.Helpers.Async;
 using Mikoto.TextHook;
 using Mikoto.Translators;
 using Mikoto.Translators.Interfaces;
@@ -12,24 +14,46 @@ namespace Mikoto.Fluent;
 
 public partial class TranslateViewModel : ObservableObject
 {
-    public GameInfo CurrentGame { get; internal set; }
+    public GameInfo CurrentGame { get; internal set; } = new GameInfo();
 
+    [ObservableProperty]
+    public partial string OriginalText { get; set; }
+    [ObservableProperty]
+    public partial string TranslateText { get; set; }
+
+    #region Notification Bar Properties
+    // 控制是否显示
+    [ObservableProperty]
+    public partial bool IsNotificationOpen { get; set; }
+
+    // 通知内容
+    [ObservableProperty]
+    public partial string NotificationMessage { get; set; }
+
+    // 通知严重程度 (Error, Warning, Informational, Success)
+    [ObservableProperty]
+    public partial InfoBarSeverity NotificationSeverity { get; set; }
+    #endregion
 
     [RelayCommand]
     public void InitializeTranslation()
     {
         // hook
         Task hookTask = App.Env.TextHookService.StartAsync(App.Env.AppSettings.Textractor_Path64, GetGamePid(CurrentGame), CurrentGame);
-        App.Env.TextHookService.MeetHookAddressMessageReceived += FilterAndDisplayData;
+        App.Env.TextHookService.MeetHookAddressMessageReceived += Hook_Output;
 
         // translatorinit
-        ITranslator? translator = TranslatorCommon.TranslatorFactory.GetTranslator(
+        _translator = TranslatorCommon.TranslatorFactory.GetTranslator(
                 App.Env.AppSettings.FirstTranslator, App.Env.AppSettings,
                 "");
 
     }
 
-    private void FilterAndDisplayData(object sender, SolvedDataReceivedEventArgs e)
+    private SolvedDataReceivedEventArgs _lastSolvedDataReceivedEventArgs = new();
+    private ITranslator? _translator;
+    private readonly AsyncLwwTask _translationTask = new();
+
+    private async void Hook_Output(object sender, SolvedDataReceivedEventArgs e)
     {
         //1.得到原句
         string? currentData = e.Data.Data; // 局部变量捕获当前快照
@@ -37,18 +61,46 @@ public partial class TranslateViewModel : ObservableObject
                                            //只处理最新的一次调用
         await _translationTask.ExecuteAsync(async () =>
         {
-            if (currentData == _lastSolvedDataReceivedEventArgs.Data?.Data) return;
-            _lastSolvedDataReceivedEventArgs = e;
-            string repairedText = CleanText(currentData);
-            Dispatcher.Invoke(SetWindowTopMost);
-
-            if (Common.AppSettings.AzureEnableAutoSpeak)
+            if (currentData == _lastSolvedDataReceivedEventArgs.Data?.Data
+            ||_translator==null)
             {
-                SpeakIfNoGameVoiceAsync(repairedText).FireAndForget();
+                return;
             }
-            TranslateText(repairedText);
+            _lastSolvedDataReceivedEventArgs = e;
+            string preProcessedText = PreProcessText(currentData);
+            string? translatedText =
+            await _translator.TranslateAsync(preProcessedText,
+                                              CurrentGame.SrcLang,
+                                              CurrentGame.DstLang);
+            //取得原文和翻译后的文本后，触发UI更新
+            Log.Debug("原文：{OriginalText}\n译文：{TranslatedText}", preProcessedText, translatedText);
+            UpdateUI(preProcessedText, translatedText, _translator.GetLastError());
         });
 
+    }
+
+    private void UpdateUI(string preProcessedText, string? translatedText, string error)
+    {
+        OriginalText = preProcessedText;
+        if (translatedText!=null)
+        {
+            TranslateText = translatedText;
+        }
+        else
+        {
+            IsNotificationOpen = true;
+            NotificationMessage = "翻译发生错误：" + error;
+            NotificationSeverity = InfoBarSeverity.Warning;
+        }
+    }
+
+    private static string PreProcessText(string? currentData)
+    {
+        if (string.IsNullOrWhiteSpace(currentData))
+        {
+            return string.Empty;
+        }
+        return currentData.Trim();
     }
 
     private static int GetGamePid(GameInfo currentGame)
---
+++ Mikoto.Helpers/Text/TextProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mikoto.Helpers.Text;
+/// <summary>
+/// 字符串处理辅助类
+/// </summary>
+public class TextProcessor
+{
+    public static string RemoveRepeatChar(string input, int repeatTime)
+    {
+        return input;
+    }
+
+}
